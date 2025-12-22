3d5ace6775b0d4e0c2a6fc79588feeccbfc9a3ac
georgemathachan <gmathachan44@gmail.com>
2025-12-22 16:02:58 +0000

    added quit server function (/quit) and solved following logic issues

--- client.py
+++ client.py
@@ -3,27 +3,36 @@ import threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 55555))
+running = True
 
 nickname = input("Choose your username: ")
 
 def receive():
-    while True:
+    global running
+    while running:
         try:
             message = client.recv(1024).decode('utf-8')
+            if not message:
+                break
             if message == 'NICK':
                 client.send(nickname.encode('utf-8'))
             else:
                 print(message)
         except:
-            print("An error occurred!")
-            client.close()
             break
+    client.close()
+
 
 
 def write():
     while True:
         message = f'{nickname}: {input("")}'
         client.send(message.encode('utf-8'))
+        if message.endswith('/quit'):
+            running = False
+            client.close()
+            print("You have left the chat.")
+            break
 thread_receive = threading.Thread(target=receive)
 thread_receive.start()
 thread_write = threading.Thread(target=write)
